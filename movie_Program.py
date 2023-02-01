de98ea7a6b36a231d3d943fa9f704d7d0ff37683
Mohamed Boudjatat <mboudjatat17@hotmail.com>
2023-02-01 16:39:34 +0000

    Added Functions to the program

--- movie_Program.py
+++ movie_Program.py
@@ -1,13 +1,32 @@
-age = input("What is your age? ")
+# age = input("What is your age? ")
+#
+# if age.isdigit() and -1 < int(age) < 177:
+#     if int(age) < 12:
+#         print("You can watch U and PG movies")
+#     elif 12 < int(age) < 15:
+#         print("You can watch 12 or 12A movies")
+#     elif 14 < int(age) < 18:
+#         print("You can watch 15 years movies")
+#     else:
+#         print("You can watch 18+ movies")
+# else:
+#     age = input("What is your age? ")
 
-if age.isdigit() and -1 < int(age) < 177:
-    if int(age) < 12:
-        print("You can watch U and PG movies")
-    elif 12 < int(age) < 15:
-        print("You can watch 12 or 12A movies")
-    elif 14 < int(age) < 18:
-        print("You can watch 15 years movies")
+
+def check_age_cinema(age):
+    if age.isdigit() and -1 < int(age) < 177:
+        if int(age) < 12:
+            return print("You can watch U and PG movies")
+        elif 12 <= int(age) < 15:
+            return print("You can watch 12 or 12A movies")
+        elif 14 < int(age) < 18:
+            return print("You can watch 15 years movies")
+        else:
+            return print("You can watch 18+ movies")
     else:
-        print("You can watch 18+ movies")
-else:
-    age = input("What is your age? ")
+        age = input("What is your age? ")
+        check_age_cinema(age)
+
+
+age = input("What is your age? ")
+check_age_cinema(age)
